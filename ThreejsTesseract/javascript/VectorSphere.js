51d970ac4c80ed7726754782e8128ee5bd46e2b6
Bjarni123 <bjarni04@gmail.com>
2023-12-05 22:20:12 +0000

    VectorSphere complete

--- ThreejsTesseract/javascript/VectorSphere.js
+++ ThreejsTesseract/javascript/VectorSphere.js
@@ -12,17 +12,24 @@ class CreateVectorSphere {
     calculatePoints() {
         // https://stackoverflow.com/questions/57123194/how-to-distribute-points-evenly-on-the-surface-of-hyperspheres-in-higher-dimensi
 
+
+
+
         // https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere
         this.points = [];
         const phi = Math.PI * (Math.sqrt(5) - 1);
 
         for (let i = 0; i < 100; i++) {
-            const y = 1 - (i / 100) * 2;
+            let y = 1 - (i / 100) * 2;
             const radius = Math.sqrt(1 - y * y);
             const theta = phi * i;
 
-            const x = Math.cos(theta) * radius;
-            const z = Math.sin(theta) * radius;
+            let x = Math.cos(theta) * radius;
+            let z = Math.sin(theta) * radius
+
+            x *= this.radius;
+            y *= this.radius;
+            z *= this.radius;
 
             this.points.push(new THREE.Vector3(x, y, z));
         }
@@ -40,30 +47,7 @@ class CreateVectorSphere {
             points.push(new THREE.Vector3(Math.cos(phi) * r * radius, y * radius, Math.sin(phi) * r * radius));
         }
         this.points = points; */
-
-
-        /* // Calculate points on a hypersphere
-        const n = 3;
-        const m = 2;
-        const r = 2;
-        const pi = Math.PI;
-        const theta = pi / n;
-        const phi = pi / m;
-        const points = [];
-        for (let i = 0; i < n; i++) {
-            const x = r * Math.cos(theta * i);
-            const y = r * Math.sin(theta * i);
-            for (let j = 0; j < m; j++) {
-                const z = r * Math.cos(phi * j);
-                const w = r * Math.sin(phi * j);
-                points.push(new THREE.Vector4(x, y, z, w));
-            }
-        }
-        this.points = points; */
         
-
-
-
         // Notkun eingahrings til að reikna punkta á hring
         /* const einingahringur = [
             0.5, Math.sqrt(2) / 2, Math.sqrt(3) / 2,
@@ -99,8 +83,22 @@ class CreateVectorSphere {
         var dotMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
         for (let i = 0; i < this.points.length; i++) {
             var dot = new THREE.Mesh( dotGeometry, dotMaterial );
-            dot.position.set(this.points[i].x * this.radius, this.points[i].y * this.radius, this.points[i].z * this.radius);
+            dot.position.set(this.points[i].x, this.points[i].y, this.points[i].z);
             this.group.add( dot );
+        }
+
+        // Draw lines between the points
+        const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+        for (let i = 0; i < this.points.length; i++) {
+            for (let j = i + 1; j < this.points.length; j++) {
+                // find the points which are closest together and draw a line between them
+                const distance = this.points[i].distanceTo(this.points[j]);
+                if (distance < 0.8) {
+                    const geometry = new THREE.BufferGeometry().setFromPoints( [this.points[i], this.points[j]] );
+                    const line = new THREE.Line( geometry, material );
+                    this.group.add( line );
+                }
+            }
         }
     }
 
